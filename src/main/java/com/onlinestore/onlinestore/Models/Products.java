82805cf982be8beeeac93a8734bb428a669dcadc
nirbhaykumarsingh <nirbhay727452@gmail.com>
2024-03-22 23:08:38 +0530

    Added Categories class

---
+++ src/main/java/com/onlinestore/onlinestore/Models/Products.java
@@ -0,0 +1,13 @@
+package com.onlinestore.onlinestore.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Products {
+    private String title;
+    private String desc;
+    private String price;
+    private String image;
+}
