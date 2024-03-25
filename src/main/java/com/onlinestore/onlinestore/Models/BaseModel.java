6c2eb5c656507db3d78790cc7838e841b5aafc0d
nirbhaykumarsingh <nirbhay727452@gmail.com>
2024-03-25 22:01:30 +0530

    Added support for MYsql DB connectivity

--- src/main/java/com/onlinestore/onlinestore/Models/BaseModel.java
+++ src/main/java/com/onlinestore/onlinestore/Models/BaseModel.java
@@ -1,12 +1,18 @@
 package com.onlinestore.onlinestore.Models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @MappedSuperclass
 public class BaseModel {
     @Id
-    @GeneratedValue
-    private long id;
+    @GeneratedValue( generator = "Generic")
+    @GenericGenerator(name = "Generic", strategy = "uuid2")
+    @Column(name = "Id1",columnDefinition = "binary(16)" ,nullable = false, updatable = false)
+    private UUID id;
 }
